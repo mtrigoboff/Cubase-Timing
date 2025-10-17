918077805e08677c0dbd6f732f56ba27b9b74c52
Michael Trigoboff <mtrigoboff@comcast.net>
2025-10-17 16:43:42 -0700

    final version candidate

--- cubase_timing.py
+++ cubase_timing.py
@@ -1,44 +1,47 @@
-TICKS_PER_16TH_NOTE = 120
-TICKS_PER_4TH_NOTE = 4 * TICKS_PER_16TH_NOTE
-TICKS_PER_BAR = 4 * TICKS_PER_4TH_NOTE
+import sys
 
-# each aublist is: [#intervals, #notes, #4thnotes]
-SPLITS = [[3, 0, 2]]
+TICKS_16TH_NOTE = 120
+TICKS_4TH_NOTE = 4 * TICKS_16TH_NOTE
+TICKS_BAR = 4 * TICKS_4TH_NOTE
+
+# each split_spec is: [#intervals, #notes, #4thnotes]
+SPLIT_SPECS = ((3, 1, 0), (3, 2, 0), (3, 4, 0),
+			   (4, 1, 0), (4, 3, 0), (4, 5, 0))
 
 class CubaseTime:
     
 	def __init__(self, ticks):
-		self._notes_4th = ticks // TICKS_PER_4TH_NOTE
-		ticks -= self._notes_4th * TICKS_PER_4TH_NOTE
-		self._notes_16th = ticks // TICKS_PER_16TH_NOTE
-		self._ticks = ticks % TICKS_PER_16TH_NOTE
+		self._notes_4th = ticks // TICKS_4TH_NOTE
+		ticks = ticks % TICKS_4TH_NOTE
+		self._notes_16th = ticks // TICKS_16TH_NOTE
+		self._ticks = ticks % TICKS_16TH_NOTE
 
-	@property
-	def notes_4th(self):			# getter
-		return self._notes_4th
-
-	@property
-	def notes_16th(self):			# getter
-		return self._notes_16th
-
-	@property
-	def ticks(self):				# getter
-		return self._ticks
-	
 	def __str__(self):
-		return f'{self.notes_4th} 4th notes, {self.notes_16th} 16th notes, {self.ticks} ticks'
+		return f'{self._notes_4th} {self._notes_16th} {self._ticks}'
 
-def main():
-	print()
-	for split in SPLITS:
-		splits = split[0]
-		notes_4th = split[1]
-		notes_16th = split[2]
+def print_split(splits, notes_4th, notes_16th):
 
-		print(f'{splits} splits, {notes_4th} 4th notes, {notes_16th} 16th notes')
+	print(f'{splits} splits, {notes_4th} 4th notes, {notes_16th} 16th notes')
 
-		ct = CubaseTime(notes_4th * TICKS_PER_4TH_NOTE + notes_16th * TICKS_PER_16TH_NOTE)
+	split_ticks = (notes_4th * TICKS_4TH_NOTE + notes_16th * TICKS_16TH_NOTE) // splits
+	for split in range(1, splits):
+		ct = CubaseTime(split_ticks * split)
 		print(ct)
 
+	print()
+
+def main(args):
+
+	if len(args) == 1:
+		for spec in SPLIT_SPECS:
+			print_split(spec[0], spec[1], spec[2])
+	elif len(args) == 4:
+		try:
+			print_split(int(args[1]), int(args[2]), int(args[3]))
+		except ValueError as e:
+			print(f'{e} is not an integer')
+	else:
+		print('need 3 integer args')
+
 if __name__ == '__main__':
-	main()
+	main(sys.argv)
